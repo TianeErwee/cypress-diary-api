2326aad3c061bc8b3be29214adef4f1002a80a31
Tiane Erwee <tiane.erwee@gmail.com>
2020-01-21 10:25:54 +0200

    update

--- cypress/integration/nav/nav.spec.js
+++ cypress/integration/nav/nav.spec.js
@@ -2,7 +2,23 @@
 
 describe('nav', () => {
   it('should open dashboard', () => {
-    cy.visit('http://localhost:4200/entries/list');
+    cy.visit('http://localhost:4200/');
     cy.wait(1000);
+    cy.url().should('include', '/dashboard');
+  });
+
+  it('should open list of entries', () => {
+    cy.get('#view-entries').click();
+    cy.url().should('include', '/entries/list');
+  });
+
+  it('should open create-entry page', () => {
+    cy.get('#create-entry').click();
+    cy.url().should('include', '/entries/create-entry');
+  });
+
+  it('should go back to list on cancel', () => {
+    cy.get('#cancel').click();
+    cy.url().should('include', '/entries/list');
   });
 });
