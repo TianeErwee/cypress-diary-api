adc1382bdbe82ac60d30181a0a17d507988a5280
Tiane Erwee <tiane.erwee@gmail.com>
2020-01-23 12:02:42 +0200

    update services

--- routes/entries.js
+++ routes/entries.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const db = mysql.createConnection ({
   host: process.env.DB_HOST,
@@ -16,105 +16,120 @@ db.connect((err) => {
   console.log('Connected to database');
 });
 
-/* GET customer listing. */
-router.get('/list', async function(req, res, next)
-{
-  db.query('SELECT * FROM entries', (err, result, fields) => {
-    if (!!err) {
-      console.log(err);
-    } else {
-      res.send({"statusCode": 200, "statusMessage": result});
-    }
+// GET LIST
+const getList = async() => {
+  return db.promise().query('SELECT * FROM entries;')
+  .then( ([rows,fields]) => {
+    return Promise.resolve(rows);
+  })
+  .catch((err) => {
+    return Promise.reject(err);
   });
-});
+}
 
-router.post('/create-entry', async (req, res, next) =>
-{
+router.get('/list', async function(req, res, next) {
+  try {
+    const entries = await getList();
+    res.send({"statusCode": 200, "statusMessage": entries});
+  } catch (err) {
+    res.status(500).send({"statusMessage": "error occurred" + err});
+  }
+});
+// END GET LIST
+
+// CREATE ENTRY
+const createEntry = async(title, content) => {
+  const query = "INSERT INTO entries (`title`, `content`, `date_created`) VALUES ('" + title + "', '"+ content + "', CURRENT_DATE);";
+  return db.promise().query(query)
+  .then( ([id]) => {
+    return Promise.resolve(id.insertId);
+  })
+  .catch((err) => {
+    return Promise.reject(err);
+  });
+}
+
+router.post('/create-entry', async (req, res, next) => {
   const body = req.body;
-	try
-	{
-    const query = "INSERT INTO entries (`title`, `content`, `date_created`) VALUES ('" + body.title + "', '"+ body.content + "', CURRENT_DATE);";
-    db.query(query, (err, result, fields) => {
-      if (!!err) {
-        console.log(err);
-      } else {
-        res.send({"statusCode": 200, "statusMessage": result});
-      }
-    });
-	}
-	catch(err)
-	{
-		if (err.name === 'ValidationError')
-		{
+	try {
+    const result = await createEntry(body.title, body.content);
+    res.send({"statusCode": 200, "statusMessage": result});
+	} catch(err) {
+		if (err.name === 'ValidationError') {
         	return res.status(400).json({ error: err.message });
 		}
-
-		// unexpected error
-		return next(err);
+		res.status(500).send({"statusMessage": "error occurred" + err});
 	}
 });
+// END CREATE ENTRY
 
-router.get('/view-entry/:id', async (req, res, next) =>
-{
-	try
-	{
-    const query = "SELECT * FROM entries WHERE id = " + req.params.id + ";";
-    db.query(query, (err, result, fields) => {
-      if (!!err) {
-        console.log(err);
-      } else {
-        res.send({"statusCode": 200, "statusMessage": result});
-      }
-    });
-	}
-	catch(err)
-	{
-		// unexpected error
-		return next(err);
+// VIEW ENTRY
+const viewEntry = async(id) => {
+  return db.promise().query("SELECT * FROM entries WHERE id = " + id + ";")
+  .then( ([rows,fields]) => {
+    if (rows.length > 0) {
+      return Promise.resolve(rows);
+    } else {
+      return Promise.reject('No matching entry');
+    }
+  })
+  .catch((err) => {
+    return Promise.reject(err);
+  });
+}
+
+router.get('/view-entry/:id', async (req, res, next) => {
+	try {
+    const result = await viewEntry(req.params.id);
+    res.send({"statusCode": 200, "statusMessage": result});
+	} catch(err) {
+		res.status(500).send({"statusMessage": "error occurred" + err});
 	}
 });
+// END VIEW ENTRY
 
-/* updates the customer by uid */
-router.put('/update-entry/:id', async (req, res, next) =>
-{
+// UPDATE ENTRY
+const updateEntry = async(title, content, id) => {
+  const query = "UPDATE entries SET `title` = '" + title + "', `content` = '" + content + "' WHERE id = " + id + ";";
+  return db.promise().query(query)
+  .then( ([id]) => {
+    return Promise.resolve(id);
+  })
+  .catch((err) => {
+    return Promise.reject(err);
+  });
+}
+
+router.put('/update-entry/:id', async (req, res, next) => {
   const body = req.body;
-	try
-	{
-    const query = "UPDATE entries SET `title` = '" + body.title + "', `content` = '" + body.content + "' WHERE id = " + req.params.id + ";";
-    db.query(query, (err, result, fields) => {
-      if (!!err) {
-        console.log(err);
-      } else {
-        res.send({"statusCode": 200, "statusMessage": result});
-      }
-    });
-	}
-	catch(err)
-	{
-		// unexpected error
-		return next(err);
+	try {
+    const result = await updateEntry(body.title, body.content, req.params.id);
+    res.send({"statusCode": 200, "statusMessage": result});
+	} catch(err) {
+		res.status(500).send({"statusMessage": "error occurred" + err});
 	}
 });
+// END UPDATE ENTRY
 
-/* removes the customer from the customer list by uid */
-router.delete('/delete-entry/:id', async (req, res, next) =>
-{
-	try
-	{
-    const query = "DELETE FROM entries WHERE id = " + req.params.id + ";";
-    db.query(query, (err, result, fields) => {
-      if (!!err) {
-        console.log(err);
-      } else {
-        res.send({"statusCode": 200, "statusMessage": result});
-      }
-    });
-	}
-	catch(err)
-	{
-		// unexpected error
-		return next(err);
+// DELETE ENTRY
+const deleteEntry = async(id) => {
+  return db.promise().query("DELETE FROM entries WHERE id = " + id + ";")
+  .then( ([id]) => {
+    return Promise.resolve(id);
+  })
+  .catch((err) => {
+    return Promise.reject(err);
+  });
+}
+
+router.delete('/delete-entry/:id', async (req, res, next) => {
+	try {
+    const result = await deleteEntry(req.params.id);
+    res.send({"statusCode": 200, "statusMessage": result});
+	} catch(err) {
+		res.status(500).send({"statusMessage": "error occurred" + err});
 	}
 });
+// END DELETE ENTRY
 
 module.exports = router;
